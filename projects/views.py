a0692fcc2cd4e329be3c667e27ddd6079bf10bbc
Drosos <drosinakis.droso1@gmail.com>
2022-12-05 18:48:02 +0200

    add i dont know what i do last timew

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+projectslist = [
+    {
+        "id": "1",
+        "title": "Ecomerce Website",
+        "description": "Fully functional ecommerce website",
+    },
+    {
+        "id": "2",
+        "title": "Portofolio website",
+        "description": "This was a project where i build out my Portofolio",
+    },
+    {
+        "id": "3",
+        "title": "Social Network",
+        "description": "Awesome open source project I am still working",
+    },
+]
 
 
 def projects(request):
-    return render(request, "projects/projects.html")
+    page = "projects"
+    number = 5
+    context = {
+        "msg": page,
+        "number": number,
+        "projects": projectslist,
+    }
+    return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
-    return render(request, "projects/single-project.html", {"count": pk})
+    projectobj = None
+    for i in projectslist:
+        if i["id"] == pk:
+            projectobj = i
+
+    return render(request, "projects/single-project.html", {"obj": projectobj})
