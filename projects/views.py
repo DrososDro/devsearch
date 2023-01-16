3bc383e6606d941edd1a452fb73aaab087deb9e9
Drosos <drosinakis.droso1@gmail.com>
2023-01-16 21:54:25 +0200

    continue with authentication

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
 
 def projects(request):
@@ -20,6 +21,7 @@ def project(request, pk):
     return render(request, "projects/single-project.html", context)
 
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     if request.method == "POST":
@@ -31,6 +33,7 @@ def createProject(request):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -43,6 +46,7 @@ def updateProject(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == "POST":
