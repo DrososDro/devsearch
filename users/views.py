2bec9c4d68a5facacff7bcbc7d1f19f7b8c8b9f1
Drosos <drosinakis.droso1@gmail.com>
2023-01-16 19:54:43 +0200

    finished with the developers views

--- users/views.py
+++ users/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 def profiles(request):
-    return render(request, "users/profiles.html")
+    profile = Profile.objects.all()
+    context = {
+        "users": profile,
+    }
+    return render(request, "users/profiles.html", context)
+
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    top_skills = profile.skill_set.exclude(description__exact="")
+    print(top_skills)
+    other_skills = profile.skill_set.filter(description="")
+    context = {
+        "user": profile,
+        "topSkills": top_skills,
+        "otherSkills": other_skills,
+    }
+    return render(request, "users/user-profile.html", context)
